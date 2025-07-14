12f654787f3ed2c74687b5facfd0f9fee4409787
aleazarGebeya <aleazar.ashagrea@gebeya.com>
2025-07-14 16:55:38 +0300

    Update files - 7/14/2025, 4:54:17 PM

--- components/footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer: React.FC = () => (
-  <footer className="bg-black py-8 text-white mt-16 text-center w-full">
-    <div className="max-w-4xl mx-auto text-sm">
-      © {new Date().getFullYear()} Jane Doe. Designed & Built with passion.
-    </div>
-  </footer>
-);
